24bf595bc40d3c43b4609e604e1c512095ca746a
hussain <root@hussain.com>
2020-09-13 17:03:41 +0530

    make changes

---
+++ robofriends/src/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function SearchBox({searchfield,searchChange}){
+    return(
+        <div className="pd2">
+            <input
+                className="pa3 mb5 ba b--green bg-lightest-blue"
+                type="search"
+                placeholder="Search Here"
+                onChange={searchChange}
+            />
+        </div>
+    )
+}
+
+export default SearchBox;
